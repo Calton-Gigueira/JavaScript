1835980bfdf8d117e00d8b53f8e10555c0f983ec
Calton Gigueira <165893905+Calton-Gigueira@users.noreply.github.com>
2024-04-08 18:29:37 +0100

    Learned more details and features about arrays and loops

--- javascript/src/08/scripts/11-todo-list.js
+++ javascript/src/08/scripts/11-todo-list.js
@@ -1,4 +1,11 @@
-const todoList = [];
+const todoList = [{
+    name: 'make dinner',
+    dueDate: '2024-04-08'
+}, 
+{
+    name: 'wash dishes',
+    dueDate: '2024-04-08'
+}];
 
 renderTodoList();
 
@@ -6,7 +13,17 @@ function renderTodoList() {
     let todoListHTML = '';
 
     for (let i = 0; i < todoList.length; i++) {
-        const html = `<p>${todoList[i]}</p>`
+        const todoObject = todoList[i];
+        //const name = todoObject.name;
+        //const dueDate = todoObject.dueDate;
+        const { name, dueDate } = todoObject; 
+        const html = `
+        <div>${name}</div> 
+        <div>${dueDate}</div> 
+        <button onclick="
+            todoList.splice(${i}, 1);
+            renderTodoList();
+        " class="delete-todo-button">Delete</button>`
         todoListHTML += html;
     }
 
@@ -17,8 +34,13 @@ function addTodo() {
     const inputElement = document.querySelector('.js-name-input');
     const name = inputElement.value;
 
-    todoList.push(name);
-    console.log(todoList);
+    const dateInputElement = document.querySelector('.js-due-date-input');
+    const dueDate = dateInputElement.value;
+
+    todoList.push({
+        name,
+        dueDate
+    });
 
     inputElement.value = '';
 
