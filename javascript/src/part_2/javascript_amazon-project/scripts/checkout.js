484fbcf1a72f8d75cee1541cb395ff8557d9d079
CaltonGigueira <caltongigueira@gmail.com>
2024-04-14 18:01:43 +0100

    Delete products from HTML when clicking delete

--- javascript/src/part_2/javascript_amazon-project/scripts/checkout.js
+++ javascript/src/part_2/javascript_amazon-project/scripts/checkout.js
@@ -16,7 +16,8 @@ cart.forEach((cartItem) => {
     });
 
     cartSummaryHTML += `
-    <div class="cart-item-container">
+    <div class="cart-item-container 
+    js-cart-item-container-${matchingProduct.id}">
         <div class="delivery-date">
             Delivery date: Tuesday, June 21
         </div>
@@ -103,5 +104,8 @@ deleteLink.forEach((link) => {
     link.addEventListener('click', () => {
         const productId = link.dataset.productId;
         removeFromCart(productId);
+
+        const conatiner = document.querySelector(`.js-cart-item-container-${productId}`);
+        conatiner.remove();
     });
 });
