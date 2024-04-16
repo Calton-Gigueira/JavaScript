436f3cf57ad0bbf54dceb46cc7f24af24657616d
CaltonGigueira <caltongigueira@gmail.com>
2024-04-16 19:22:35 +0100

    Save data and generate HTML for delivery options

--- javascript/src/part_2/javascript_amazon-project/data/cart.js
+++ javascript/src/part_2/javascript_amazon-project/data/cart.js
@@ -1,12 +1,16 @@
+import { deliveryOptions } from "./deliveryOptions";
+
 export let cart = JSON.parse(localStorage.getItem('cart'));
 
 if (!cart) {
     cart = [{
         productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-        quantity: 2
+        quantity: 2,
+        deliveryOptionId: '1'
     }, {
         productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-        quantity: 1
+        quantity: 1,
+        deliveryOptionId: '2'
     }];
 }
 
@@ -28,7 +32,8 @@ export function addToCart(productId, quantitySelector) {
     } else {
         cart.push({
             productId: productId,
-            quantity: Number(quantitySelector.value)
+            quantity: Number(quantitySelector.value),
+            deliveryOptionId: '1' 
         });
     }
 
