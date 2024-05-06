59844accdc25cec953ae8250d0c509a70275da05
CaltonGigueira <caltongigueira@gmail.com>
2024-05-06 13:54:35 +0100

    Convert from promisesvto async await

--- javascript/src/part_2/javascript_amazon-project/scripts/checkout.js
+++ javascript/src/part_2/javascript_amazon-project/scripts/checkout.js
@@ -5,6 +5,22 @@ import { loadCart } from "../data/cart.js";
 // import '../data/cart-class.js';
 // import '../data/backend-practice.js';
 
+async function loadPage() {
+  await loadProductsFetch();
+
+  const value = await new Promise((resolve) => {
+    loadCart(() => {
+      resolve('value3');
+    });
+  });
+
+  renderOrderSummary();
+  renderPymentSummary();
+}
+
+loadPage();
+
+/*
 Promise.all([
   loadProductsFetch(),
   new Promise((resolve) => {
@@ -17,6 +33,7 @@ Promise.all([
     renderOrderSummary();
     renderPymentSummary();
 });
+*/
 
 /*
 new Promise((resolve) => {
