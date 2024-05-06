ac6473a5597a563560f293921ce494be57979bc6
CaltonGigueira <caltongigueira@gmail.com>
2024-05-06 15:42:49 +0100

    Create order using backend

--- javascript/src/part_2/javascript_amazon-project/scripts/checkout/paymentSummary.js
+++ javascript/src/part_2/javascript_amazon-project/scripts/checkout/paymentSummary.js
@@ -2,6 +2,7 @@ import { cart } from "../../data/cart.js";
 import { getProduct } from "../../data/products.js";
 import { getDeliveryOption } from "../../data/deliveryOptions.js";
 import { formatCurrency } from "../utils/money.js";
+import { addOrdder } from "../../data/orders.js";
 
 export function renderPymentSummary() {
     let productPriceCents = 0;
@@ -59,11 +60,33 @@ export function renderPymentSummary() {
             </div>
         </div>
 
-        <button class="place-order-button button-primary">
+        <button class="place-order-button button-primary js-place-order">
             Place your order
         </button>
     `;
 
     document.querySelector('.js-payment-summary')
         .innerHTML = paymentSummaryHTML;
+
+    document.querySelector('.js-place-order')
+        .addEventListener('click', async () => {
+            try {
+                const response = await fetch('https://supersimplebackend.dev/orders', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type' : 'application/json'
+                    },
+                    body: JSON.stringify({
+                        cart: cart
+                    })
+                });
+    
+                const order = await response.json();
+                addOrdder(order);
+            } catch(error) {
+                console.log("Unexpected error. Please try again later.");
+            }
+
+            window.location.href = 'orders.html';
+        });
 }
